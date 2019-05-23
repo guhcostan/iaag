80b8be11afcbb5f382ef7f94d6ab15d5ada29234
Gustavo Costa <gustavo.neves.lemaf@gmail.com>
2019-05-23 14:23:53 -0300

    criado IA

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,71 @@
 <template>
-  <div id="app">
-  </div>
+	<div id="app">
+		<div class="formulario">
+			<p>Tamanho dos genes</p>
+			<input v-model="tam_genes">
+			<br>
+			<p>Tamanho da Populacao</p>
+			<input v-model="tam_pop">
+			<br>
+			<p>Tamanho do torneio</p>
+			<input v-model="tam_tornenio">
+			<br>
+			<p>Numero de geraçoes</p>
+			<input v-model="geracoes">
+			<br>
+			<p>Probabilidade de mutacao</p>
+			<input v-model="prob_mut">
+			<br>
+			<p>Probabilidade de cruzamento</p>
+			<input v-model="prob_cruz">
+			<br>
+			<p></p>
+			<button v-on:click="startIA">Iniciar IA</button>
+		</div>
+		<span class="console" style="white-space: pre-line" v-html="console"></span>
+	</div>
 </template>
 
 <script>
 
-export default {
-  name: 'app',
-  mounted() {
+import AG from './components/AG';
 
-  }
+export default {
+	name: 'app',
+	data() {
+		return {
+			tam_genes: 100,
+			tam_pop: 30,
+			tam_tornenio: 20,
+			geracoes: 20,
+			prob_mut: 0.01,
+			prob_cruz: 0.7,
+			console: ''
+		}
+	},
+	methods: {
+		async startIA() {
+			this.console = await AG.StartAG(this.tam_genes, this.tam_pop, this.tam_tornenio, this.geracoes, this.prob_mut, this.prob_cruz);
+		}
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: #2c3e50;
+		display: flex;
+		margin-top: 60px;
+	}
+
+	.console{
+		margin-left: 50px;
+		color: white;
+		min-width: 80%;
+		background-color: black;
+		padding: 50px;
+	}
 </style>
--- src/components/AG.js
+++ src/components/AG.js
@@ -1,37 +1,148 @@
 export default class AG {
 
-	static criarPopulacao(nPopulacao){
+	static populacao = [];
+	static tam_genes = [];
+	static tam_pop = [];
+	static tam_tornenio = [];
+	static geracoes = [];
+	static prob_mut = [];
+	static prob_cruz = [];
 
-	}
+	static criarPopulacao() {
 
-	static avaliaPopulacao(populacao){
+		for (var i = 0; i < this.tam_pop; ++i) {
+			var individuo = [];
 
+			for (var j = 0; j < this.tam_genes; ++j) {
+				var num = Math.round(Math.random());
+				individuo.push(num);
+			}
 
-	}
-
-	static selecionaPopulacao(populacao){
-		var novaPopulacao = [];
-		populacao.forEach(individuo => {
-			novaPopulacao.push(individuo)
-		});
-
-		return novaPopulacao;
-	}
-
-	static reproduzPopulacao(populacao){
-
-	}
-
-	static async StartAG(nPopulacao) {
-
-		var passo = 0;
-		var populacao = this.criarPopulacao(nPopulacao);
-		this.avaliaPopulacao(populacao)
-		while(1){
-			passo++;
-			this.selecionaPopulacao(populacao)
-			this.reproduzPopulacao(populacao)
-			this.avaliaPopulacao(populacao)
+			this.populacao.push(individuo);
 		}
 	}
+
+	static mutacao(individuo) {
+		var gene = Math.floor(Math.random()) * this.tam_genes;
+
+		if (individuo[gene] === 0) {
+			individuo[gene] = 1;
+		} else {
+			individuo[gene] = 0;
+		}
+	}
+
+	static cruzamento(indice_pai1, indice_pai2, filho) {
+
+		var ponto = Math.floor(Math.random() * this.tam_genes);
+
+		for (var i = 0; i <= ponto; ++i) {
+			filho.push(this.populacao[indice_pai1][i]);
+		}
+		for (var j = ponto + 1; j < this.tam_genes; ++j) {
+			filho.push(this.populacao[indice_pai2][j]);
+		}
+	}
+
+	static obterMelhor() {
+		var indice_melhor = 0;
+		var score_melhor = this.obterPontuacao(this.populacao[0]);
+
+		for (var i = 1; i < this.tam_pop; ++i) {
+			var score = this.obterPontuacao(this.populacao[i]);
+			if (score > score_melhor) {
+				indice_melhor = i;
+				score_melhor = score;
+			}
+		}
+
+		return indice_melhor;
+	}
+
+	static obterPontuacao(individuo) {
+		var soma = 0;
+
+		for (var i = 0; i < this.tam_genes; ++i) {
+			soma += individuo[i];
+		}
+		return soma;
+
+	}
+
+	static async StartAG(tam_genes = 100, tam_pop = 30, tam_torneio = 20, geracoes = 20, prob_mut = 0.01, prob_cruz = 0.7) {
+
+		this.populacao = []
+		var consoleOutput = ''
+		this.tam_genes = tam_genes;
+		this.tam_pop = tam_pop;
+		this.tam_tornenio = tam_torneio;
+		this.geracoes = geracoes;
+		this.prob_mut = prob_mut;
+		this.prob_cruz = prob_cruz;
+
+		this.criarPopulacao();
+
+		for (var i = 0; i < geracoes; ++i) {
+			for (var j = 0; j < tam_torneio; ++j) {
+
+				// calcula a probabilidade de cruzamento
+				var prob = Math.round(Math.random() * 100 + 1) / 100;
+
+				if (prob < this.prob_cruz) {
+					// escolhe dois pais
+					var indice_pai1 = Math.floor(Math.random() * tam_pop);
+					var indice_pai2;
+
+					// garante que os índices dos pais não são iguais
+					do {
+						indice_pai2 = Math.floor(Math.random() * tam_pop);
+					}
+					while (indice_pai1 === indice_pai2);
+
+					var filho = [];
+
+					// aplica o cruzamento de 1 ponto
+					this.cruzamento(indice_pai1, indice_pai2, filho);
+
+					// calcula a probabilidade de mutação
+					prob = Math.round(Math.random() * 100 + 1) / 100;
+
+					if (prob < prob_mut) {
+						this.mutacao(filho);
+					}
+
+					var score_pai = this.obterPontuacao(this.populacao[indice_pai1]);
+					var score_filho = this.obterPontuacao(filho);
+
+					/*
+						se a pontuação (score) do filho for melhor do
+						que o pai1, então substitui o pai 1 pelo filho
+					*/
+					if (score_filho > score_pai) {
+						// faz a cópia dos genes do filho para o pai
+						for (var k = 0; k < tam_genes; k++)
+							this.populacao[indice_pai1][k] = filho[k];
+					}
+				}
+			}
+			consoleOutput += '<b>Geracao ' + (i + 1) + '</b>\n';
+			consoleOutput += 'Melhor: \n'
+
+			var indice_melhor = this.obterMelhor();
+			var score_melhor = this.obterPontuacao(this.populacao[indice_melhor]);
+
+			consoleOutput += this.populacao[indice_melhor]
+
+			consoleOutput += '\nPontuacao: <b>' + score_melhor + '</b>\n\n'
+
+			// verifica se encontrou a solução ótima global
+			if (score_melhor === tam_genes) {
+				break;
+			}
+		}
+
+		return consoleOutput;
+
+	}
 }
+
